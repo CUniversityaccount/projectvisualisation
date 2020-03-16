a08ac71a294deb5d31e6a386ab42d048a389a5bc
CUniversityaccount <44020631+CUniversityaccount@users.noreply.github.com>
2020-03-16 20:30:51 +0100

    Update main.js

--- javascriptfiles/main.js
+++ javascriptfiles/main.js
@@ -13,7 +13,7 @@ function loadWebpageLayout() {
             .enter()
             .append("a")
             .attr("href", function (dp) {
-              return "/" + dp.toLowerCase() + ".html";
+              return "../" + dp.toLowerCase() + ".html";
             })
             .text(function (dp) {
               if (dp === "Specific") {
